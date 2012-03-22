06c247ecf5ef49d250c209f65499822d900d7290
antoniojunior87@gmail.com <antoniojunior87@gmail.com@f82a4a1a-0178-e507-e433-53658de85126>
2012-03-22 21:15:04 +0000

    Conclusão da infra.

--- trunk/fontes/fastdelivery-service/src/main/java/br/com/fastdelivery/infra/service/GenericService.java
+++ trunk/fontes/fastdelivery-service/src/main/java/br/com/fastdelivery/infra/service/GenericService.java
@@ -10,88 +10,109 @@ import org.hibernate.criterion.Order;
 /**
  * @author Junior
  */
-public abstract class GenericSercice<T> implements IService<T> {
+public abstract class GenericService<T> implements IService<T> {
 
+    @Override
     public void salvar(T objeto) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void salvarOuAtualizar(T objeto) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public T carregarLoad(Integer id) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public T carregarGet(Integer id) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public T carregarLoad(Serializable id) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public T carregarGet(Serializable id) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public T findByIdAndLock(Serializable id) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public List<T> listarTodos() throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void atualizar(T objeto) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void remover(T objeto) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public T buscarEntidadePorExemplo(T exemplo) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public List<T> buscarPorNamedQuery(String namedQuery) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public List<T> buscarPorNamedQuery(String namedQuery, Map<String, Object> parametros) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public List<T> buscarPorNativeQuery(String sql) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public List<T> listarPorExemplo(T exemplo) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public List<T> listarPorExemplo(T objeto, MatchMode matchMode) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void executarNamedQuery(String namedQuery, Map<String, Object> parametros) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public List<T> listarPorCriteria(DetachedCriteria detachedCriteria, Order... order) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public T buscarPorCriteria(DetachedCriteria detachedCriteria) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public T buscarEntidadePorNamedQuery(String namedQuery, Map<String, Object> parametros) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    @Override
     public void salvarEmLote(List<T> pLista) throws Exception {
         throw new UnsupportedOperationException("Not supported yet.");
     }
